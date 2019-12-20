61b4ab5f2faa48c8a6cc795760e385062dc9636f
Truman Craig <tscgnw@mst.edu>
2019-12-20 13:09:14 -0600

    Middle of Redux

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: "mongodb+srv://tscgnw:Tmann1998@cluster0-924eb.mongodb.net/test?retryWrites=true&w=majority",
+  keys: {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true
+  }
+}
