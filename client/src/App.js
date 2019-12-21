1b30ab9decfcded9747bc0eb906d2349016accd6
Truman Craig <tscgnw@mst.edu>
2019-12-20 20:27:37 -0600

    finished redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import store from './store';
 
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal'
+import { Container } from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -13,7 +15,10 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
--- client/src/store.js
+++ client/src/store.js
@@ -4,11 +4,11 @@ import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleWare = {thunk};
+const middleWare = [thunk];
 
 const store = createStore(rootReducer, initialState, compose(
   applyMiddleware(...middleWare),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+));
 
 export default store;
